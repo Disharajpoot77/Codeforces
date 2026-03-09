a4ba9d04e548dbfd91024cab70348d1be64031b3
Disha rajpoot <rajpootdisha42@gmail.com>
2026-03-09 23:19:29 +0530

    neww

---
+++ Maxmedian.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+#include<algorithm>
+
+int main(){
+	int n,k;
+	cin>>n>>k;
+	int arr[n];
+	for(int i=0; i<n; i++){
+		cin>>arr[i];
+	}
+	sort(arr,arr+n);
+	int median=n/2;
+	for(int i=0; i<k; i++){
+		// cout<<arr[median];
+		// median+1;
+		if(median+1<n && arr[median]<arr[median+1]){
+			arr[median]++;
+			// cout<<"seee"<<arr[median]<<endl;
+		}
+		else {
+			arr[median]++;
+		}
+	
+	}
+	cout<<"final"<<arr[median]<<endl;
+}
+ 
