559e516ae2474817663aee3215eec248db754d8c
Disha rajpoot <rajpootdisha42@gmail.com>
2026-03-12 23:25:08 +0530

    Sliding window

---
+++ Books.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n,t;
+	cin>>n>>t;
+	int arr[n];
+	for(int i=0; i<n; i++){
+		cin>>arr[i];
+	}
+    int l=0;
+	long long sum=0;
+	int maxsum=0;
+	for(int i=0; i<n; i++){
+         sum+=arr[i];
+		 while(sum>t){
+			sum-=arr[l];
+			l++;
+		 }
+		 maxsum=max(maxsum,i-l+1);	
+	}
+	cout<<maxsum<<endl;
+	return 0;
+}
---
+++ Fence.cpp
@@ -0,0 +1,40 @@
+
+#include<iostream>
+using namespace std;
+int main(){
+    int n,k;
+    cin>>n>>k;
+    int arr[n];
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+
+    }
+
+    int sum=0;
+    int index = 0;   
+	 int count=0;
+    int current=0;
+    for(int i=0; i<n; i++){
+        if(count<k){
+            count++;
+            current+=arr[i];
+            sum+=arr[i];
+          
+        }
+        else{
+            current+=arr[i];
+            current-=arr[i-k];
+             if(current<sum){
+                sum=current;
+				index=i-k+1;
+			 }
+        }
+    }
+    
+    cout<<index+1<<endl;
+    // cout<<sum;
+    return 0;
+}
+      
+
+ 
