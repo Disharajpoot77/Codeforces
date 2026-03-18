1d4c88fad46e449eabed4dac5a85c6bb98a1e46a
Disha rajpoot <rajpootdisha42@gmail.com>
2026-03-18 22:33:06 +0530

    newww

--- Kefa_company.cpp
+++ Kefa_company.cpp
@@ -12,30 +12,28 @@ int main()
 		cin >> arr[i].first >> arr[i].second;
 	}
 	sort(arr, arr + n);
-	long long left = 0;
-	long long right = n - 1;
-	long long  maxfact = 0;
-	if (arr[right].first - arr[left].first <= d)
-	{
-		for (int i = 0; i < n; i++)
-		{
-			maxfact += arr[i].second;
+     long long max=0;
+	 long long current=0;
+	 long long m=0;
+	 
+	 long long limit=arr[m].first+d;
+	 
+	//  cout<<"limit"<<limit<<endl;
+	 for(int i=0; i<n; i++){
+		if(arr[i].first<limit){
+			current+=arr[i].second;	
+			cout<<"checkkkk"<<arr[i].first<<" "<<arr[i].second;
 		}
-	}
-	else if (arr[right].first - arr[left].first > d)
-	{
-		if (arr[right].second > arr[left].second)
-		{
-			maxfact += arr[right].second;
-			left++;
-		}
-		else
-		{
-			maxfact += arr[left].second;
-			right--;
-		}
-	}
-	cout << maxfact;
+	 }
+	
+	// arr[i]=arr[i+1].first;
+     if(current>max){
+		max=current;
+	 }
+
+     
+	 cout<<"final"<<max;
+	
 
 	return 0;
 }
